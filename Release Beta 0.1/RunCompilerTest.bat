a258e7c393fa80fe2dcc7ff0f24c5b3e67f9b53b
MyhreAndersen <o.myhre@gmail.com>
2018-04-27 10:01:14 +0200

    Release Beta 0.1

--- Release Beta 0.1/RunCompilerTest.bat
+++ Release Beta 0.1/RunCompilerTest.bat
@@ -3,3 +3,7 @@ java -jar .\simula.jar
 pause
 java -cp %SIMULA_HOME%\simula.jar -jar .\simula.jar .\HelloWord.sim
 pause
+java -cp .\simula.jar -jar .\HelloWord.jar
+pause
+java -jar .\HelloWord.jar
+pause
